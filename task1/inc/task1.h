862b561847756599a5218761792bf8129ee30a6a
Marcin Kubsik <m.kubsik@samsung.com>
2018-12-06 17:10:21 +0100

    [master] clean up task1

---
+++ task1/inc/task1.h
@@ -0,0 +1,19 @@
+#ifndef __task1_H__
+#define __task1_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "task1"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.task1"
+#endif
+
+#endif /* __task1_H__ */
